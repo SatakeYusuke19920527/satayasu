44f3388e7046100ccd95ae97755869d2f7110538
mkayuri2009 <yuuy93543@gmail.com>
2024-02-10 20:57:50 +0900

    hai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <div>
-        <h1 style={{textAlign:'center'}}>さたやす</h1>
+      <div style={{textAlign:'center'}}>
+        <h1 >さたやす</h1>
         <label>休む理由<input type="text"></input></label>
       </div>
   );
