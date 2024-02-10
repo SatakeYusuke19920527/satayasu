695bea2411aa0d217595ffcecfd8d80d0b14dd7d
mkayuri2009 <yuuy93543@gmail.com>
2024-02-10 20:55:27 +0900

    できました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
       <div>
         <h1 style={{textAlign:'center'}}>さたやす</h1>
+        <label>休む理由<input type="text"></input></label>
       </div>
   );
 }
